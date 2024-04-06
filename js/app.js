a7f2fe5ddba63e3ce5255b8d66a8ab03e546b77e
SaMuRaIdevv <mukhridd1nchoriyev2007@gmail.com>
2024-04-06 13:59:01 +0500

    commit

---
+++ js/app.js
@@ -0,0 +1,136 @@
+"use strict"
+
+// FOR-1
+
+// let k = 3,
+// n = 7;
+
+// for (let i = 0; i < n; i++) {
+//     console.log(k);
+    
+// }
+
+// FOR-2
+
+// let a = 13, b = 5, count = 0;
+
+// if (a < b) {
+//     for (let i = a; i <= b; i++) {
+//         console.log(i);
+//         count++
+        
+//     }
+// } else{
+//     for (let i = b; i <= a; i++) {
+//         console.log(i);
+//         count++
+//     }
+// }
+
+// console.log(count);
+
+// FOR-3
+
+// let a = 5, b = 18 , count = 0
+
+// if (a < b) {
+//     for (let i = b - 1; i > a; i--) {
+//         console.log(i);
+//         count++
+//     }
+// }else {
+//     for (let i = a - 1; i > b; i--) {
+//         console.log(i);
+//         count++
+//     }
+// }
+
+// console.log(count);
+
+// FOR-4
+
+// let a = 1 , b = 10, price = 30
+
+// for (let i = a; i <= b; i++) {
+//     let narx = i*price
+//     console.log(narx);
+// }
+
+// FOR-5
+
+// let a = 0.1 , b = 1 , price = 100
+
+// for (let i = a; i <= b; i+=0.1 ) {
+//     let narx = i*price
+//     console.log(narx);
+    
+// }
+
+// FOR-6
+
+// let a = 1 , b = 2 , price = 100
+
+// for (let i = a; i <= b; i+=0.2 ) {
+//     let narx = i*price
+//     console.log(narx);
+    
+// }
+
+// FOR-7
+
+// let a = 1, b = 5, count = 0
+
+// for (let i = a; i <= b; i++) {
+//     count+=1
+//     console.log(count);
+// }
+
+// FOR-8
+
+// let a = 6 , b = 10 , count=1 ;
+
+// if (a < b) {
+//     for (let i = a ; i <= b; i++) {  
+//         count*=i
+//     }
+// } else{
+//     for (let i = b; i <= a; i++) {
+//         count*=i  
+//     }
+// }
+// console.log(count);
+
+// FOR-9
+
+// let a = 5 , b = 14 , count=0 ;
+
+// if (a < b) {
+//     for (let i = a ; i <= b; i++) {  
+//         let result= i*i
+//         count+=result
+//     }
+// } else{
+//     for (let i = b; i <= a; i++) {
+//         let result= i*i
+//         count+=result
+//     }
+// }
+// console.log(count);
+
+// FOR-10
+
+// let n = 100, count = 0
+
+// for(let i = 1; i <= n; i++){
+//     count += 1/i
+// }
+// console.log(count);
+
+// FOR-11
+
+// let n = 2 , count = 0
+
+// for(let i = 0; i <= n; i++){
+//     count += Math.pow(n+i,2)
+// }
+// console.log(count);
